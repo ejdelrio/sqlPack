ed2e4262bac7c57b76227d7caf5761f3a816437e
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-17 16:06:32 -0800

    Created a rough draft for simple sql query :D

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const sqlPack = module.exports = {};
+
+sqlPack.currentQuery = '';
+
+sqlPack.query = function() {
+  sqlPack.currentQuery = '';
+  return this;
+};
+
+sqlPack.select = function(...props) {
+  if(!props) throw new Error('Invalid SELECT parameter in SQL query');
+  if(!props.length === 0) throw Error('Pameter contains no values.');
+
+  this.currentQuery += 'SELECT ';
+
+  for(let i = 0; i < props.length; i++) {
+    this.currentQuery += `${props[i]}`
+    if(i !== props.length - 1) this.currentQuery += ',';
+    this.currentQuery += ' ';
+  }
+  return this;
+};
+
+sqlPack.from = function(table) {
+  sqlPack.currentQuery += `FROM ${table} `;
+  return this;
+};
+
+sqlPack.where = function(obj) {
+  if(typeof obj !== 'object') throw new Error('WHERE parameter must be an object');
+
+  this.currentQuery += 'WHERE ';
+  let keysArr = Object.keys(obj);
+  for (let i = 0; i < keysArr.length; i++) {
+    let key = keysArr[i];
+    let val = obj[key];
+    this.currentQuery += `${key}=${val}`;
+    if(i !== obj.length - 1) this.currentQuery += ',';
+    this.currentQuery += ' ';
+  }
+  return this;
+};
